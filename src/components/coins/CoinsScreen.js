568ce4eddb4a2aadb70cf403a3c8a35860029c8f
Ismael Ponce <ImDevPonce@outlook.com>
2020-09-23 23:38:12 -0500

    nuestra primera pantalla con stack navigator

---
+++ src/components/coins/CoinsScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const CoinsScreen = () => {
+  return (
+    <View>
+      <Text>Coins Screen</Text>
+    </View>
+  );
+};
+
+export default CoinsScreen;
